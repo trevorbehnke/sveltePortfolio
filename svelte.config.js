fbe9c509ff6d04801d55bf616f46e4653df6865d
trevor <me@trevorbehnke.com>
2021-09-26 13:46:05 -0700

    configured tailwind & static adapter

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
+};
+
+export default config;
