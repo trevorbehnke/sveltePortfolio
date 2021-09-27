22e7219e28618e197d0991f28c515a7a1bbca176
trevor <me@trevorbehnke.com>
2021-09-27 14:59:53 -0700

    refactor: :art: refactored pcard styling

--- src/lib/components/PCard.svelte
+++ src/lib/components/PCard.svelte
@@ -9,12 +9,12 @@
 </script>
 
 <div class="my-6 flex flex-wrap w-full items-center justify-center bg-green-500 ">
-	<div class="bg-red-500 w-full sm:w-1/2 h-96 flex items-center">
+	<div class="bg-red-500 w-full sm:w-1/2 p-4 flex items-center">
 		<figure>
 			<img class="rounded-xl" src="./FakerDex.png" alt="FakerDex Landing Page" />
 		</figure>
 	</div>
-	<div class="bg-blue-500 w-full sm:w-1/2 flex flex-col justify-center px-4">
+	<div class="bg-blue-500 w-full sm:w-1/2 flex flex-col justify-center p-4">
 		<div class="mb-10">
 			<h1 class="text-center">{title}</h1>
 		</div>
