600a1c631db140f43b2cbd0277a6135ce69f0b82
trevor <me@trevorbehnke.com>
2021-09-27 14:23:59 -0700

    created Button.svelte component/ configured props & styling

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -7,6 +7,10 @@
 	title="FakerDex"
 	description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, voluptates aperiam voluptate
 tempora at minus hic ut vero totam dolorem."
+	siteTitle="Website"
+	siteLink="https://google.com"
+	sourceTitle="Source"
+	sourceLink="https://bing.com"
 />
 <PCard title="Project 2" />
 <PCard title="Project 3" />
