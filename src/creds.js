644dd6f0ecffbdc4f2aa76ce4cc2494bb87a81a7
trevor <me@trevorbehnke.com>
2021-10-18 18:16:04 -0700

    feat: :zap: configured new creds page

---
+++ src/creds.js
@@ -0,0 +1,20 @@
+let creds = [
+  {
+    src: './ace.webp',
+    alt: 'associate cloud engineer'
+  },
+  {
+    src: './pca.webp',
+    alt: 'professional cloud architect'
+  },
+  {
+    src: './sac.webp',
+    alt: 'california state university sacramento'
+  },
+  {
+    src: './capm.webp',
+    alt: 'certified associate in project management'
+  }
+]
+
+export default creds
